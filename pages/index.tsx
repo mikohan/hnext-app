5705864c963b336849dcc95fbe4af06d9d0d52a2
mikohan <angara99@gmail.com>
2020-11-10 12:41:35 +0300

    Added css module for the component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,16 @@
 import Link from 'next/link';
 import React from 'react';
+import styles from '../styles/index.module.css';
+
 export default function Home() {
   return (
     <React.Fragment>
-      <div>
-        <div className="CoolClass">
-          <div className="container">
-            <h1>Some cool stuf</h1>
-            <Link href="/nicepage">
-              <h2 className="title">Link</h2>
-            </Link>
-            <style jsx>
-              {`
-                .coolClass {
-                  color: red;
-                  display: flex;
-                  height: 100vh;
-                  justify-content: center;
-                }
-                .title {
-                  font-size: 4rem;
-                  color: teal;
-                  cursor: pointer;
-                  text-decroration: underline;
-                  margin-left: 5rem;
-                }
-                .container {
-                  width: 80%;
-                }
-                .coolClass {
-                  background-color: red;
-                }
-              `}
-            </style>
-          </div>
+      <div className={styles.coolClass}>
+        <div className={styles.container}>
+          <h1>Some cool stuf</h1>
+          <Link href="/nicepage">
+            <h2 className={styles.title}>Link</h2>
+          </Link>
         </div>
       </div>
     </React.Fragment>
