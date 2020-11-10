145c8477723559e3ef0ca519623c977f19fd9952
mikohan <angara99@gmail.com>
2020-11-10 13:02:52 +0300

    Sass working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import styles from '../styles/index.module.css';
+import styles from '../styles/index.module.scss';
 
 export default function Home() {
   return (
